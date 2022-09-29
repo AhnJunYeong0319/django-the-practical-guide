693f29278446bbed9162321f1b2f256786115903
Junyeong Ahn <hewas321@gmail.com>
2022-09-29 19:55:07 +0900

    September 29

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin): # allows us to set various options that will be reflected in admin page.
+    #readonly_fields = ("slug",)
+    prepopulated_fields = {"slug" : ("title",)} # somewhat redundant to save() in models.py
+    list_filter = ("author", "rating", )
+    list_display = ("title", "author",)
+
+admin.site.register(Book, BookAdmin)
--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -6,22 +6,29 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Author(models.Model):
+    first_name = models.CharField(max_length = 100)
+    last_name = models.CharField(max_length = 100)
+
+
+
 class Book(models.Model):
     # search on google for this keywords; django model field reference
     title = models.CharField(max_length = 50) 
     rating = models.IntegerField(validators = [MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null = True, max_length = 100) # consider blank = True instead of null = True
+    author = models.ForeignKey(Author, on_delete = models.CASCADE, null = True) # special field / means a pointer
     is_bestselling = models.BooleanField(default = False)
-    slug = models.SlugField(default = "", null = False)
+    slug = models.SlugField(default = "", blank = True,
+    editable = True, null = False, db_index = True) # editable = False <- this means that this field cannot be editted at all in administration page
 
     id = models.AutoField # don't have to set this cuz this is automatically runned whithin Django database class definition
     
     def get_absolute_url(self):
-        return reverse("book-detail", args = [self.id]) # same to the url tag in DTL
+        return reverse("book-detail", args = [self.slug]) # same to the url tag in DTL
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+    #def save(self, *args, **kwargs):
+    #    self.slug = slugify(self.title)
+    #    super().save(*args, **kwargs)
 
     def __str__(self): # whenever a book is output to the console
         return f"({self.title}) ({self.rating})"
